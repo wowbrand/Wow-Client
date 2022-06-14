2e34d4b6710c42d4114425b6106e8bffd9bb403c
nayab fatima <syedanayab2014@hotmail.com>
2022-06-14 17:32:15 +0200

    inintialized the React typescript app,created a navigation bar

---
+++ src/components/Banner.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export interface IAppProps {
+}
+
+export function Banner(props: IAppProps) {
+  return (
+    <div>
+      <img src="https://images.unsplash.com/photo-1445116572660-236099ec97a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2FmZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60" alt=""></img>
+
+    </div>
+  );
+}
---
+++ src/components/Navigationbar.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { Navbar, Nav } from "react-bootstrap";
+import "./Navigationbar.css"
+
+
+export interface Props {
+}
+
+export function Navigationbar(props: Props) {
+  return (
+    <div>
+      <Navbar bg="myBar" variant="dark">
+        <Nav.Link className="navbar-brand" href="/">
+          WOWBRAND
+          <i className="fas fa-leaf"></i>
+        </Nav.Link>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav.Link className="navbar-brand" href="List">
+            Brand Places
+          </Nav.Link>
+          <Nav.Link className="navbar-brand" href="register">
+            Register
+          </Nav.Link>
+          <Nav.Link className="navbar-brand" href="login">
+            Login
+          </Nav.Link>
+          <Nav.Link className="navbar-brand" href="profile">
+            <i className="fas fa-user"></i>
+          </Nav.Link>
+        </Navbar.Collapse>
+      </Navbar>
+
+
+
+
+
+    </div >
+  );
+}
